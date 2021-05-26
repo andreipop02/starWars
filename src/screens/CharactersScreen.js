18ccce2960c137707dda28c7b5fb5ce816e8ca20
Andrei <andreipop02@yahoo.com>
2021-05-26 11:57:04 +0300

    Promise in Character Screens

--- src/screens/CharactersScreen.js
+++ src/screens/CharactersScreen.js
@@ -2,18 +2,20 @@ import React, {useState, useEffect} from 'react';
 import {View, Text, FlatList, TouchableOpacity} from 'react-native';
 import {fetchResults} from '../api/index';
 import {BASE_URL} from '../constants/url';
-import homeStyle from '../styles/HomeScreenStyles';
 
 const Characters = ({route, navigation}) => {
   const url = route.params.results;
 
   const [people, setPeople] = useState([]);
 
-   const getNames = async () => {
-      const name = await fetchResults(`${url[1]}`);
-      setPeople(name);
-    };
-  
+  const getNames = () => {
+    Promise.all(
+      url.map(index => {
+        return fetchResults(index);
+      }),
+    ).then(values => setPeople(values));
+  };
+
   useEffect(() => {
     getNames();
   }, []);
@@ -21,12 +23,12 @@ const Characters = ({route, navigation}) => {
   return (
     <View style={{justifyContent: 'space-between'}}>
       <FlatList
-        data={people.name}
+        data={people}
         keyExtractor={() => Math.random() * 9999}
         renderItem={({item}) => {
           return (
             <View style={{alignItems: 'center'}}>
-              <Text style={{marginTop: 20, marginLeft: 20}}>{item}</Text>
+              <Text style={{marginTop: 20, marginLeft: 20}}>{item.name}</Text>
             </View>
           );
         }}
